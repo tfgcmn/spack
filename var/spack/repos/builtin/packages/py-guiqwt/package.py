511cddcd71f5402c1499ea6cf593300d2230d72e
Eric Müller <mueller@kip.uni-heidelberg.de>
2017-01-26 10:49:14 +0100

    fixup Add py-guiqwt

--- var/spack/repos/builtin/packages/py-guiqwt/package.py
+++ var/spack/repos/builtin/packages/py-guiqwt/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class PyGuiqwt(Package):
+class PyGuiqwt(PythonPackage):
     """guiqwt is a set of tools for curve and image plotting (extension to PythonQwt)"""
 
     homepage = "https://github.com/PierreRaybaut/guiqwt"
@@ -33,14 +33,9 @@ class PyGuiqwt(Package):
 
     version('3.0.2', 'b49cd9706f56eb5d519390ba709d8c8c')
 
-    extends('python')
-
     depends_on('py-setuptools', type='build')
-    depends_on('py-numpy@1.3:',       type=nolink)
-    depends_on('py-scipy@0.7:',       type=nolink)
-    depends_on('py-guidata@1.7.0:',   type=nolink)
-    depends_on('py-pythonqwt@0.5.0:', type=nolink)
-    depends_on('py-pillow',           type=nolink)
-
-    def install(self, spec, prefix):
-        setup_py('install', '--prefix={0}'.format(prefix))
+    depends_on('py-numpy@1.3:',       type=('build', 'run'))
+    depends_on('py-scipy@0.7:',       type=('build', 'run'))
+    depends_on('py-guidata@1.7.0:',   type=('build', 'run'))
+    depends_on('py-pythonqwt@0.5.0:', type=('build', 'run'))
+    depends_on('py-pillow',           type=('build', 'run'))
