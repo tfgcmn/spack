d626eeeaf647d1e335c31dcee9c57926f22d42bb
Eric Müller <mueller@kip.uni-heidelberg.de>
2017-01-26 10:49:16 +0100

    fixup Add py-requests

--- var/spack/repos/builtin/packages/py-requests/package.py
+++ var/spack/repos/builtin/packages/py-requests/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class PyRequests(Package):
+class PyRequests(PythonPackage):
     """Python HTTP for Humans."""
 
     homepage = "http://python-requests.org"
@@ -33,34 +33,29 @@ class PyRequests(Package):
 
     version('2.11.1', 'ad5f9c47b5c5dfdb28363ad7546b0763')
 
-    extends('python')
-
     depends_on('py-setuptools',              type='build')
-    depends_on('py-alabaster@0.7.7:',        type=nolink)
+    depends_on('py-alabaster@0.7.7:',        type=('build', 'run'))
     # ECM: replaced explicit version by open range
-    depends_on('py-babel@2.2.0:',            type=nolink)
-    depends_on('py-coverage@4.0.3:',         type=nolink)
-    depends_on('py-decorator@4.0.9:',        type=nolink)
-    depends_on('py-docutils@0.12:',          type=nolink)
-    depends_on('py-flask@0.10.1:',           type=nolink)
-    depends_on('py-httpbin@0.4.1:',          type=nolink)
-    depends_on('py-itsdangerous@0.24:',      type=nolink)
-    depends_on('py-jinja2@2.8:',             type=nolink)
-    depends_on('py-markupsafe@0.23:',        type=nolink)
-    depends_on('py-py@1.4.31:',              type=nolink)
-    depends_on('py-pygments@2.1.1:',         type=nolink)
-    depends_on('py-pysocks@1.5.6:',          type=nolink)
-    depends_on('py-pytest@2.8.7:',           type=nolink)
-    depends_on('py-pytest-cov@2.2.1:',       type=nolink)
-    depends_on('py-pytest-httpbin@0.2.0:',   type=nolink)
-    depends_on('py-pytest-mock@0.11.0:',     type=nolink)
-    depends_on('py-pytz@2015.7:',            type=nolink)
-    depends_on('py-six@1.10.0:',             type=nolink)
-    depends_on('py-snowballstemmer@1.2.1:',  type=nolink)
-    depends_on('py-sphinx@1.3.5:',           type=nolink)
-    depends_on('py-sphinx-rtd-theme@0.1.9:', type=nolink)
-    depends_on('py-werkzeug@0.11.4:',        type=nolink)
-    depends_on('py-wheel@0.29.0:',           type=nolink)
-
-    def install(self, spec, prefix):
-        setup_py('install', '--prefix={0}'.format(prefix))
+    depends_on('py-babel@2.2.0:',            type=('build', 'run'))
+    depends_on('py-coverage@4.0.3:',         type=('build', 'run'))
+    depends_on('py-decorator@4.0.9:',        type=('build', 'run'))
+    depends_on('py-docutils@0.12:',          type=('build', 'run'))
+    depends_on('py-flask@0.10.1:',           type=('build', 'run'))
+    depends_on('py-httpbin@0.4.1:',          type=('build', 'run'))
+    depends_on('py-itsdangerous@0.24:',      type=('build', 'run'))
+    depends_on('py-jinja2@2.8:',             type=('build', 'run'))
+    depends_on('py-markupsafe@0.23:',        type=('build', 'run'))
+    depends_on('py-py@1.4.31:',              type=('build', 'run'))
+    depends_on('py-pygments@2.1.1:',         type=('build', 'run'))
+    depends_on('py-pysocks@1.5.6:',          type=('build', 'run'))
+    depends_on('py-pytest@2.8.7:',           type=('build', 'run'))
+    depends_on('py-pytest-cov@2.2.1:',       type=('build', 'run'))
+    depends_on('py-pytest-httpbin@0.2.0:',   type=('build', 'run'))
+    depends_on('py-pytest-mock@0.11.0:',     type=('build', 'run'))
+    depends_on('py-pytz@2015.7:',            type=('build', 'run'))
+    depends_on('py-six@1.10.0:',             type=('build', 'run'))
+    depends_on('py-snowballstemmer@1.2.1:',  type=('build', 'run'))
+    depends_on('py-sphinx@1.3.5:',           type=('build', 'run'))
+    depends_on('py-sphinx-rtd-theme@0.1.9:', type=('build', 'run'))
+    depends_on('py-werkzeug@0.11.4:',        type=('build', 'run'))
+    depends_on('py-wheel@0.29.0:',           type=('build', 'run'))
