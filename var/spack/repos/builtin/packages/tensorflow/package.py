f69fb95ee5596bd03ca1670a5942d9edb9b97a15
Eric Müller <mueller@kip.uni-heidelberg.de>
2017-02-17 16:38:55 +0100

    Add tensorflow@1.0.0-rc2 (and support for cuda)

--- var/spack/repos/builtin/packages/tensorflow/package.py
+++ var/spack/repos/builtin/packages/tensorflow/package.py
@@ -7,10 +7,12 @@ class Tensorflow(Package):
     homepage = "https://www.tensorflow.org"
     url      = "https://github.com/tensorflow/tensorflow/archive/v0.10.0.tar.gz"
 
-    version('0.10.0', 'b75cbd494d61a809af5ef25d7fba561b')
+    version('1.0.0-rc2', 'a058a7e0ba2b9761cf2420c82d520049')
+    version('0.10.0',    'b75cbd494d61a809af5ef25d7fba561b')
 
-    depends_on('bazel@0.3.1:',         type='build')
     depends_on('swig',                 type='build')
+    depends_on('bazel@0.4.4:',         type='build', when='@1:')
+    depends_on('bazel@0.3.1:',         type='build', when='@:1')
 
     extends('python')
     depends_on('py-numpy@1.8.2:',      type=('build', 'run'))
@@ -21,6 +23,8 @@ class Tensorflow(Package):
 
     patch('url-zlib.patch', when='@0.10.0')
 
+    patch('crosstool.patch', when='@1.0.0-rc2') # auch auf 0.10.0 wenn mit cuda!
+
     # FIXME: tensorflow pulls in a lot more dependencies...
     # e.g. eigen3 already exists as a spack package!?
 
@@ -29,26 +33,44 @@ class Tensorflow(Package):
 
     variant('cuda', default=False,
             description='Enable CUDA Support')
+
     depends_on('cuda', when='+cuda')
+    depends_on('cudnn', when='+cuda')
 
     def install(self, spec, prefix):
         if '+gcp' in spec:
             env['TF_NEED_GCP'] = '1'
         else:
             env['TF_NEED_GCP'] = '0'
-        env['PYTHON_BIN_PATH'] = str(which('python'))
-        env['SWIG_PATH'] = str(which('swig'))
-        env['GCC_HOST_COMPILER_PATH'] = str(which('gcc'))
 
-        assert '~cuda' in spec # for CUDA fix env variables below
+        env['PYTHON_BIN_PATH'] = str(spec['python'].prefix.bin) + '/python'
+        env['SWIG_PATH'] = str(spec['swig'].prefix.bin)
+        env['GCC_HOST_COMPILER_PATH'] = spack_cc
+
         if '+cuda' in spec:
             env['TF_NEED_CUDA'] = '1'
+            env['TF_CUDA_VERSION'] = str(spec['cuda'].version)
+            env['CUDA_TOOLKIT_PATH'] = str(spec['cuda'].prefix)
+            env['TF_CUDNN_VERSION'] = str(spec['cudnn'].version)[0]
+            env['CUDNN_INSTALL_PATH'] = str(spec['cudnn'].prefix)
+            env['TF_CUDA_COMPUTE_CAPABILITIES'] = '3.5,5.2'
         else:
             env['TF_NEED_CUDA'] = '0'
-        env['TF_CUDA_VERSION'] = '' # spec['cuda'].version ?
-        env['CUDA_TOOLKIT_PATH'] = '' # spec['cuda'].prefix ?
-        env['TF_CUDNN_VERSION'] = ''
-        env['CUDNN_INSTALL_PATH'] = ''
+            env['TF_CUDA_VERSION'] = ''
+            env['CUDA_TOOLKIT_PATH'] = ''
+            env['TF_CUDNN_VERSION'] = ''
+            env['CUDNN_INSTALL_PATH'] = ''
+
+        if self.spec.satisfies('@1.0.0-rc2:'):
+            env['CC_OPT_FLAGS'] = ' '
+            env['TF_NEED_JEMALLOC'] = '0'
+            env['TF_NEED_HDFS'] = '0'
+            env['TF_ENABLE_XLA'] = '0'
+            env['PYTHON_LIB_PATH'] = self.module.site_packages_dir
+            env['TF_NEED_OPENCL'] = '0'
+
+        # clean bazel build cache
+        which('bazel')('clean')
 
         configure()
         if '+cuda' in spec:
