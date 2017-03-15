88da80d84fb3a90cba667d328fee49cb4dbacfb9
Eric Müller <mueller@kip.uni-heidelberg.de>
2017-03-15 16:04:20 +0100

    Modified py-pytables

--- var/spack/repos/builtin/packages/py-pytables/package.py
+++ var/spack/repos/builtin/packages/py-pytables/package.py
@@ -28,8 +28,9 @@ from spack import *
 class PyPytables(PythonPackage):
     """PyTables is a package for managing hierarchical datasets and designed to
     efficiently and easily cope with extremely large amounts of data."""
+
     homepage = "http://www.pytables.org/"
-    url      = "https://github.com/PyTables/PyTables/archive/v.3.2.2.tar.gz"
+    url      = "https://pypi.io/packages/source/t/tables/tables-3.3.0.tar.gz"
 
     version('3.3.0', '056c161ae0fd2d6e585b766adacf3b0b',
             url='https://github.com/PyTables/PyTables/archive/v3.3.0.tar.gz')
@@ -41,6 +42,22 @@ class PyPytables(PythonPackage):
     depends_on('py-cython', type=('build', 'run'))
     depends_on('py-six', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
+    depends_on('lzo')
+    depends_on('bzip2')
+    # depends_on('c-blosc')
+    # FIXME:
+    #    .../lib/python2.7/site-packages/tables-3.3.0-py2.7-linux-x86_64.egg/tables/filters.py in <module>()
+    #         36
+    #         37 all_complibs = ['zlib', 'lzo', 'bzip2', 'blosc']
+    #    ---> 38 all_complibs += ['blosc:%s' % cname for cname in blosc_compressor_list()]
+    #         39
+    #         40
+    #
+    #    tables/utilsextension.pyx in tables.utilsextension.blosc_compressor_list (tables/utilsextension.c:6977)()
+    #
+    #    UnicodeEncodeError: 'ascii' codec can't encode characters in position 0-16: ordinal not in range(128)
+    #
+    # => use sources bundled with pytables for now
 
     def setup_environment(self, spack_env, run_env):
         spack_env.set('HDF5_DIR', self.spec['hdf5'].prefix)
