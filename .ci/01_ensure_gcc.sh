b9165758be646f44aa0f7f403afdb90247cda745
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2018-01-26 13:46:47 +0100

    fix module generation and add umask

--- .ci/01_ensure_gcc.sh
+++ .ci/01_ensure_gcc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+umask 0002
+
 rm -f $HOME/.spack/linux/compilers.yaml
 spack fetch -D gcc@7.2.0
 srun -p compile -c8 -t6:00:00 spack install --show-log-on-error gcc@7.2.0
--- .ci/02_install.sh
+++ .ci/02_install.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
+umask 0002
 
 modulename=$1
 modulespec=$2
+compiler=$3
 
-spack fetch -D $module
+spack fetch -D "$modulespec"
 srun -p compile -c8 -t6:00:00 spack install --only dependencies --show-log-on-error "$modulespec"
 # fancy bash-magic to get the hash of the installed package
 newhash=$(spack install --show-log-on-error "$modulespec" | tail -n1 | tr "-" "\n" | tail -n1)
 
-viewname=views/$modulename_$iteration
+viewname="views/${modulename}_$BUILD_ID"
 spack view --dependencies yes hardlink -i $viewname /$newhash
 spack view --dependencies no hardlink -i $viewname $compiler
 
-bash 00_module.sh $viewname $modulename
+DIR="$( cd "$( dirname "$0}" )" && pwd )"
+bash $DIR/00_module.sh $viewname "modules/${modulename}_$BUILD_ID"
