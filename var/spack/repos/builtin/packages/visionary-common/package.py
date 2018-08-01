0f160fe9bfb1be3aa921c06b23d64a46283dfb3f
Andreas Baumbach <healther.astro@gmail.com>
2018-08-01 10:10:51 +0200

    add the-silver-searcher for MK
    Change-Id: If61466999db8431ae1a0745919f3959877c4e318

--- var/spack/repos/builtin/packages/visionary-common/package.py
+++ var/spack/repos/builtin/packages/visionary-common/package.py
@@ -48,6 +48,10 @@ class VisionaryCommon(Package):
     # TODO: as soon as a MetaPackage-concept has been merged, please update this package
     version('1.0', '372ce038842f20bf0ae02de50c26e85d', url='https://github.com/electronicvisions/spack/archive/v0.8.tar.gz')
 
+    depends_on('py-git-review')
+    depends_on('the-silver-searcher')
+    depends_on('zsh')
+
     def install(self, spec, prefix):
         mkdirp(prefix.etc)
         # store a copy of this package.
