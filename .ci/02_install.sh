a23d74295ac71269ba9dd266835988928c73eb64
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2018-08-06 18:16:58 +0200

    Fix bash command to find hash of package builds
    Change-Id: Ic03ff777aac00c1c3cf2131cccf61bbcd520745a

--- .ci/02_install.sh
+++ .ci/02_install.sh
@@ -14,7 +14,11 @@ else
     srun -p jenkins -c8 -t6:00:00 spack install --only dependencies --show-log-on-error "$modulespec"
 fi
 # fancy bash-magic to get the hash of the installed package
-newhash=$(spack install --show-log-on-error -j4 "$modulespec" | tail -n1 | tr "-" "\n" | tail -n1)
+# For most builds the last output line of `spack install` reports the installation directory,
+# which contains the hash of the package as the last entry after a '-'.
+# If the package was previously installed as a dependency there will be a line added containing
+# "marking the package explicit" and the second last line contains the hash.
+newhash=$(spack install --show-log-on-error -j4 "$modulespec" | tail -n2 | grep -v "marking the package explicit" | tail -n1 | tr "-" "\n" | tail -n1)
 
 viewname="views/${modulename}_$BUILD_ID"
 spack view --dependencies yes hardlink -i $viewname /$newhash
