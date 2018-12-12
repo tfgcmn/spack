68564a73ee4dff0889cd8eb6c584ddad22d305f6
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2018-12-12 15:22:05 +0100

    visionary-simulation: Add h5py
    Change-Id: I1140e082c7e46667b5cd58fca52a61f11c76e431

--- var/spack/repos/builtin/packages/visionary-simulation/package.py
+++ var/spack/repos/builtin/packages/visionary-simulation/package.py
@@ -42,6 +42,7 @@ class VisionarySimulation(Package):
 
     depends_on('nest')
     depends_on('visionary-nest')
+    depends_on('py-h5py')
     depends_on('py-ipython')
     depends_on('py-pandas')
     depends_on('py-pyyaml')
