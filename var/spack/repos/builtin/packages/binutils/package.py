5d133c7758d713e6f1c1082ad27d5e0d580d2bc2
Eric Müller <mueller@kip.uni-heidelberg.de>
2019-02-11 14:14:59 +0100

    Update binutils
    Change-Id: Ic90ec7b9e0aba3180a65ea762669a4151ecacad2

--- var/spack/repos/builtin/packages/binutils/package.py
+++ var/spack/repos/builtin/packages/binutils/package.py
@@ -23,18 +23,21 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import glob
 
 
 class Binutils(AutotoolsPackage):
     """GNU binutils, which contain the linker, assembler, objdump and others"""
 
     homepage = "http://www.gnu.org/software/binutils/"
-    url      = "https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.bz2"
+    url      = "https://ftpmirror.gnu.org/binutils/binutils-2.28.tar.bz2"
 
+    version('2.31.1', 'ffcc382695bf947da6135e7436b8ed52d991cf270db897190f19d6f9838564d0')
     version('2.29.1', '9af59a2ca3488823e453bb356fe0f113')
     version('2.28', '9e8340c96626b469a603c15c9d843727')
     version('2.27', '2869c9bf3e60ee97c74ac2a6bf4e9d68')
     version('2.26', '64146a0faa3b411ba774f47d41de239f')
+    version('2.25.1', sha256='b5b14added7d78a8d1ca70b5cb75fef57ce2197264f4f5835326b0df22ac9f22')
     version('2.25', 'd9f3303f802a5b6b0bb73a335ab89d66')
     version('2.24', 'e0f71a7b2ddab0f8612336ac81d9636b')
     version('2.23.2', '4f8fa651e35ef262edc01d60fb45702e')
@@ -44,29 +47,31 @@ class Binutils(AutotoolsPackage):
             description="enable plugins, needed for gold linker")
     variant('gold', default=True, description="build the gold linker")
     variant('libiberty', default=False, description='Also install libiberty.')
+    variant('nls', default=True, description='Enable Native Language Support')
+    variant('headers', default=False, description='Install extra headers (e.g. ELF)')
 
-    patch('cr16.patch')
+    patch('cr16.patch', when='@:2.29.1')
     patch('update_symbol-2.26.patch', when='@2.26')
 
     depends_on('zlib')
+    depends_on('gettext', when='+nls')
 
-    depends_on('m4', type='build')
-    depends_on('flex', type='build')
-    depends_on('bison', type='build')
-    depends_on('gettext')
+    # Prior to 2.30, gold did not distribute the generated files and
+    # thus needs bison, even for a one-time build.
+    depends_on('m4', type='build', when='@:2.29.99 +gold')
+    depends_on('bison', type='build', when='@:2.29.99 +gold')
 
     def configure_args(self):
         spec = self.spec
 
         configure_args = [
-            '--with-system-zlib',
             '--disable-dependency-tracking',
             '--disable-werror',
-            '--enable-interwork',
             '--enable-multilib',
             '--enable-shared',
             '--enable-64-bit-bfd',
             '--enable-targets=all',
+            '--with-system-zlib',
             '--with-sysroot=/',
         ]
 
@@ -79,6 +84,11 @@ class Binutils(AutotoolsPackage):
         if '+libiberty' in spec:
             configure_args.append('--enable-install-libiberty')
 
+        if '+nls' in spec:
+            configure_args.append('--enable-nls')
+        else:
+            configure_args.append('--disable-nls')
+
         # To avoid namespace collisions with Darwin/BSD system tools,
         # prefix executables with "g", e.g., gar, gnm; see Homebrew
         # https://github.com/Homebrew/homebrew-core/blob/master/Formula/binutils.rb
@@ -86,3 +96,17 @@ class Binutils(AutotoolsPackage):
             configure_args.append('--program-prefix=g')
 
         return configure_args
+
+    @run_after('install')
+    def install_headers(self):
+        # some packages (like TAU) need the ELF headers, so install them
+        # as a subdirectory in include/extras
+        if '+headers' in self.spec:
+            extradir = join_path(self.prefix.include, 'extra')
+            mkdirp(extradir)
+            # grab the full binutils set of headers
+            install_tree('include', extradir)
+            # also grab the headers from the bfd directory
+            for current_file in glob.glob(join_path(self.build_directory,
+                                                    'bfd', '*.h')):
+                install(current_file, extradir)
