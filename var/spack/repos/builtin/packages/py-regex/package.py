69e6828486aab34644129c07844859a95f292260
Eric Müller <mueller@kip.uni-heidelberg.de>
2019-02-14 12:36:36 +0100

    Add new versions to py-regex
    Change-Id: I3ea790ac3b0503d3996aefdebfa1ecb418376ffe

--- var/spack/repos/builtin/packages/py-regex/package.py
+++ var/spack/repos/builtin/packages/py-regex/package.py
@@ -31,6 +31,8 @@ class PyRegex(PythonPackage):
     homepage = "https://pypi.python.org/pypi/regex/"
     url      = "https://pypi.io/packages/source/r/regex/regex-2017.07.11.tar.gz"
 
+    version('2019.02.07', 'b887ba08ec75564463791013cb1465f1')
+    version('2018.01.10', '318e5c65deeb05a00f0b63d2189d2e7e')
     version('2017.07.11', '95f81ebb5273c7ad9a0c4d1ac5a94eb4')
 
     depends_on('py-setuptools', type='build')
