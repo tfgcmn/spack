47e69f303c8c9399309138f83cbcf5675df59d6c
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-02-22 16:07:02 +0100

    Revert "Add new versions to py-requests"
    This reverts commit 0220c6ab8f7a1694476618176b351f085531a420.
    
    With 0220c6ab8f7a1694476618176b351f085531a420, `spack spec` calls
    enter an infinite loop that seemingly re-reads compiler-configuration
    indefinitely.
    
    Change-Id: Iee83770eca621edae990758ee22348e09a327bb8

--- var/spack/repos/builtin/packages/py-requests/package.py
+++ var/spack/repos/builtin/packages/py-requests/package.py
@@ -42,24 +42,12 @@ class PyRequests(PythonPackage):
         'requests.packages.urllib3.contrib._securetransport'
     ]
 
-    version('2.21.0', '1bcd0e0977c3f8db1848ba0e2b7ab904')
-    version('2.19.1', '6c1a31afec9d614e2e71a91ee6ca2878')
-    version('2.18.4', '081412b2ef79bdc48229891af13f4d82')
-    version('2.17.3', 'b1b8e6412e11e00e12adbb7786c3f2a3')
-    version('2.16.5', 'cca4f82209373759f5799fc0df6aca23')
-    version('2.15.1', '1bcf7a286fa26e40a5a397f06ece00e4')
     version('2.14.2', '4c3c169ed67466088a2a6947784fe444')
     version('2.13.0', '921ec6b48f2ddafc8bb6160957baf444')
     version('2.11.1', 'ad5f9c47b5c5dfdb28363ad7546b0763')
     version('2.3.0',  '7449ffdc8ec9ac37bbcd286003c80f00')
 
     depends_on('py-setuptools', type='build')
-    depends_on('py-chardet@3.0.2:3.0.99', type=('build', 'run'), when='@2.16.5:')
-    depends_on('py-idna@2.5:2.9.99', type=('build', 'run'), when='@2.21.0:')
-    depends_on('py-idna@2.5:2.5.99', type=('build', 'run'), when='@2.16.5:')
-    depends_on('py-urllib3@1.21.1:1.24.99', type=('build', 'run'), when='@2.21.0:')
-    depends_on('py-urllib3@1.21.1:1.21.99', type=('build', 'run'), when='@2.16.5:')
-    depends_on('py-certifi@2017.4.17:', type=('build', 'run'))
 
     # TODO: Add a 'test' deptype
     # depends_on('py-pytest@2.8.0:',        type='test')
