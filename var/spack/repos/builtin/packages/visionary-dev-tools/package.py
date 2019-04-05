5f840dcf01b22f3e6a14d505da4be8fbc3ed70ac
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-04-05 14:17:25 +0200

    dev-tools: add wget
    Change-Id: Iaf7b9f13e46e00bd2e91c1edef7a8603de6b88aa

--- var/spack/repos/builtin/packages/visionary-dev-tools/package.py
+++ var/spack/repos/builtin/packages/visionary-dev-tools/package.py
@@ -99,6 +99,7 @@ class VisionaryDevTools(Package):
     depends_on('verilator')
     depends_on('vim +python +ruby +perl +cscope +huge +x')
     depends_on('visionary-xilinx')
+    depends_on('wget')
     depends_on('yaml-cpp+shared')
     depends_on('zsh')
 
