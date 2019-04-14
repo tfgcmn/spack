85c16fab490c0f31a1a888b8e1492180ff2c52a0
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-04-14 19:40:12 +0200

    py-grpcio: Fix ares.h not found
    Change-Id: Ifbc0c12402e18c8aeb11fa8e3a5de4183f5f694a

--- var/spack/repos/builtin/packages/py-grpcio/package.py
+++ var/spack/repos/builtin/packages/py-grpcio/package.py
@@ -32,16 +32,16 @@ class PyGrpcio(PythonPackage):
 
     version('1.16.0', 'f7793df1c31a89a89d18966790c740b4')
 
-    depends_on('cares',                    	type=('build', 'run'))
-    depends_on('zlib',               		type=('build', 'run'))
-    depends_on('openssl@1.0.2:',    		type=('build', 'run'))
-    depends_on('py-setuptools',     		type='build')
-    depends_on('py-six@1.10:',      		type=('build', 'run'))
-    depends_on('py-futures@2.2.0:', 		type=('build', 'run'))
-    depends_on('py-enum34@1.0.4:',  		type=('build', 'run'))
-    depends_on('py-sphinx@1.3:',    		type=('build', 'run'))
-    depends_on('py-sphinx-rtd-theme@0.1.8:', 	type=('build', 'run'))
-    depends_on('py-cython@0.23:',         	type=('build', 'run'))
+    depends_on('cares',                      type=('build', 'run'))
+    depends_on('zlib',                       type=('build', 'run'))
+    depends_on('openssl@1.0.2:',             type=('build', 'run'))
+    depends_on('py-setuptools',              type='build')
+    depends_on('py-six@1.10:',               type=('build', 'run'))
+    depends_on('py-futures@2.2.0:',          type=('build', 'run'))
+    depends_on('py-enum34@1.0.4:',           type=('build', 'run'))
+    depends_on('py-sphinx@1.3:',             type=('build', 'run'))
+    depends_on('py-sphinx-rtd-theme@0.1.8:', type=('build', 'run'))
+    depends_on('py-cython@0.23:',            type=('build', 'run'))
 
     def setup_environment(self, spack_env, run_env):
         spack_env.set('GRPC_PYTHON_BUILD_WITH_CYTHON', '1')
@@ -49,4 +49,5 @@ class PyGrpcio(PythonPackage):
         spack_env.set('GRPC_PYTHON_BUILD_SYSTEM_ZLIB', '1')
         spack_env.set('GRPC_PYTHON_BUILD_SYSTEM_CARES', '1')
         spack_env.append_flags('LDFLAGS', self.spec['cares'].libs.search_flags)
+        spack_env.append_flags('CPATH', self.spec['cares'].prefix.include)
         spack_env.append_flags('LDFLAGS', self.spec['zlib'].libs.search_flags)
