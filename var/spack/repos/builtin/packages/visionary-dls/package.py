652991454e84318b79ab53fe8dcf51e806a492a8
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-05-22 13:01:33 +0200

    visionary-dls: add boost context/coroutine
    Boost context/coroutine are not built by default anymore, hence the
    visionary-dls stack fails to build without these changes.
    
    Change-Id: Ib6609ea094a038655b92a155f998be77a90dd156

--- var/spack/repos/builtin/packages/visionary-dls/package.py
+++ var/spack/repos/builtin/packages/visionary-dls/package.py
@@ -50,7 +50,7 @@ class VisionaryDls(Package):
     variant('gccxml', default=False)
 
     # depends_on('libusb-1.0')  external dependency
-    depends_on('boost@1.69.0+graph+icu+mpi+python+numpy')
+    depends_on('boost@1.69.0+graph+icu+mpi+python+numpy+coroutine+context cxxstd=14')
     depends_on('cereal')
     depends_on('cppcheck')
     depends_on('doxygen+graphviz')
