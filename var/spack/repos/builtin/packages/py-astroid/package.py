0de440758a933f7f565c5baa08423f3c4bbeebbd
Eric Müller <mueller@kip.uni-heidelberg.de>
2019-06-18 17:36:48 +0200

    Fix py-pylint python constraints
    Change-Id: I98e020383d80bee51ec817e97314d60a58d996ee

--- var/spack/repos/builtin/packages/py-astroid/package.py
+++ var/spack/repos/builtin/packages/py-astroid/package.py
@@ -14,12 +14,16 @@ class PyAstroid(PythonPackage):
     url      = "https://github.com/PyCQA/astroid/archive/astroid-1.4.5.tar.gz"
 
     version('2.2.0', sha256='7e289d0aa4a537b4aa798bd609fdf745de0f3c37e6b67642ed328e1482421a6d')
+    version('1.6.6', 'ba7b90edc9f441b3be49ded3019d00ed')
     # version('1.5.3', '6f65e4ea8290ec032320460905afb828') # has broken unit tests
     version('1.4.5', '7adfc55809908297ef430efe4ea20ac3')
     version('1.4.4', '8ae6f63f6a2b260bb7f647dafccbc796')
     version('1.4.3', '4647159de7d4d0c4b1de23ecbfb8e246')
     version('1.4.2', '677f7965840f375af51b0e86403bee6a')
     version('1.4.1', 'ed70bfed5e4b25be4292e7fe72da2c02')
+
+    depends_on('python@2.7:2.8,3.4:', when='@:1.999.999')
+    depends_on('python@3.4:', when='@2:')
 
     depends_on('py-lazy-object-proxy')
     depends_on('py-six')
--- var/spack/repos/builtin/packages/py-pylint/package.py
+++ var/spack/repos/builtin/packages/py-pylint/package.py
@@ -13,18 +13,20 @@ class PyPylint(PythonPackage):
     url      = "https://pypi.io/packages/source/p/pylint/pylint-1.6.5.tar.gz"
 
     version('2.3.0', sha256='ee80c7af4f127b2a480d83010c9f0e97beb8eaa652b78c2837d3ed30b12e1182')
-    # version('1.7.2', '27ee752cdcfacb05bf4940947e6b35c6') # see dependencies
+    version('1.9.4', sha256='ee1e85575587c5b58ddafa25e1c1b01691ef172e139fc25585e5d3f02451da93')
+    version('1.7.2', '27ee752cdcfacb05bf4940947e6b35c6') # see dependencies
     version('1.6.5', '31da2185bf59142479e4fa16d8a9e347')
     version('1.4.3', '5924c1c7ca5ca23647812f5971d0ea44')
     version('1.4.1', 'df7c679bdcce5019389038847e4de622')
 
-    extends('python', ignore=r'bin/pytest')
-    depends_on('py-astroid', type=('build', 'run'))
+    depends_on('python@3.4.0:', when='@2.0.0:')
+    depends_on('python@2.7.0:2.8.0', when='@:1.999.999')
+    depends_on('py-astroid@:2.99.99', type=('build', 'run'))
     # note there is no working version of astroid for this
     depends_on('py-astroid@1.5.1:', type=('build', 'run'), when='@1.7:')
     depends_on('py-six', type=('build', 'run'))
-    depends_on('py-isort@4.2.5:')
-    depends_on('py-mccabe')
+    depends_on('py-isort@4.2.5:5')
+    depends_on('py-mccabe@0.6.0:0.6.99')
     depends_on('py-editdistance')
     depends_on('py-setuptools@17.1:', type='build')
     # depends_on('py-setuptools-scm@1.15.0:', type='build')
